f7f2be965a7488c549d230be42cc1c7e29c7f2dd
Bruno Vitte <bruno_vitte@hotmail.com>
2026-02-12 22:26:58 -0600

    Format: Adjust indentation and spacing in premake5.lua for consistency

--- premake5.lua
+++ premake5.lua
@@ -1,42 +1,42 @@
 project "IMGUI"
-kind "StaticLib"
-language 'C++'
-cppdialect 'C++latest'
+	kind "StaticLib"
+	language 'C++'
+	cppdialect 'C++latest'
 
-targetdir("bin/" .. outputdir .. "/%{prj.name}")
-objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir("bin/" .. outputdir .. "/%{prj.name}")
+	objdir("bin-int/" .. outputdir .. "/%{prj.name}")
 
-files {
-    "imconfig.h",
-    "imgui.h",
-    "imgui.cpp",
-    "imgui_draw.cpp",
-    "imgui_internal.h",
-    "imgui_widgets.cpp",
-    "imstb_rectpack.h",
-    "imstb_textedit.h",
-    "imstb_truetype.h",
-    "imgui_tables.cpp",
-    "backends/imgui_impl_opengl3.cpp",
-    "backends/imgui_impl_glfw.cpp",
-    "imgui_demo.cpp"
-}
+	files {
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_tables.cpp",
+		"backends/imgui_impl_opengl3.cpp",
+		"backends/imgui_impl_glfw.cpp",
+		"imgui_demo.cpp"
+	}
 
-includedirs {
-    "./",
-    "../GLFW/include"
-}
+	includedirs {
+		"./",
+		"../GLFW/include"
+	}
 
-filter "system:macosx"
-architecture "ARM64"
+	filter "system:macosx"
+	architecture "ARM64"
 
-filter "system:windows"
-architecture "x86_64"
+	filter "system:windows"
+	architecture "x64"
 
-filter "configurations:Debug"
-runtime "Debug"
-symbols "on"
+	filter "configurations:Debug"
+	runtime "Debug"
+	symbols "on"
 
-filter "configurations:Release"
-runtime "Release"
-optimize "on"
+	filter "configurations:Release"
+	runtime "Release"
+	optimize "on"
